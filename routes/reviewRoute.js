eec542db7fa4321f7b49deeabc457354505c744b
Nadee07 <nadeerawithana2k20@gmail.com>
2025-01-11 21:59:29 +0530

    Done implementing reviews backend

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -1,21 +1,21 @@
 import express from "express";
-import { addReview , deleteReview, getReviews } from "../controllers/reviewController.js";
+import { addReview , approveReview, deleteReview, getReviews } from "../controllers/reviewController.js";
 
 
 const reviewRouter = express.Router();
 reviewRouter.post("/",addReview)
 reviewRouter.get("/",getReviews)
+reviewRouter.delete( "/:email",deleteReview)
+reviewRouter.put( "/approve/:email",approveReview)
 
-reviewRouter.delete( "/:email", deleteReview)
 
-reviewRouter.get("/approved",(req,res)=>{
+/*reviewRouter.get("/approved",(req,res)=>{
     console.log("This is approved route");
 })
 
 reviewRouter.get("/:email",(req,res)=>{
     console.log("This is email route");
-})
-
+}) */
 
 
 export default reviewRouter;
